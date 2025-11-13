90cd33f6f3f4cd21bda2ae14f499e6a7a52d3244
Thiago-Stein <thiagomes.stein@edu.unifil.br>
2025-11-13 20:05:24 -0300

    Add files via upload

--- script10.js
+++ script10.js
@@ -1,4 +1,4 @@
-function configurarPopup(botaoId, popupId, fecharId) {
+  function configurarPopup(botaoId, popupId, fecharId) {
   const botao = document.getElementById(botaoId);
   const popup = document.getElementById(popupId);
   const fechar = document.getElementById(fecharId);
@@ -12,39 +12,43 @@ function configurarPopup(botaoId, popupId, fecharId) {
   });
 }
 
-configurarPopup("V1", "popup1", "fechar1");
-configurarPopup("V2", "popup2", "fechar2");
-configurarPopup("V3", "popup3", "fechar3");
+  configurarPopup("V1", "popup1", "fechar1");
+  configurarPopup("V2", "popup2", "fechar2");
+  configurarPopup("V3", "popup3", "fechar3");
+  
+  const campoResposta = document.getElementById("resposta");
+  const botaoEnviar = document.getElementById("botaoEnviar");
+  const corpoFase = document.getElementById('corpo-fase10');
+  const respostaCorreta = 386;
 
-const campoResposta = document.getElementById("resposta");
-const botaoEnviar = document.getElementById("botaoEnviar");
-const respostaCorreta = 386; 
+  const mensagemErro = document.createElement('p');
+  mensagemErro.id = 'mensagemErro';
+  mensagemErro.textContent = 'Resposta incorreta. Tente novamente!';
+  corpoFase.appendChild(mensagemErro);
 
-botaoEnviar.addEventListener('click', verificarResposta);
-inputResposta.addEventListener('keypress', function(event) {
+  botaoEnviar.addEventListener('click', verificarResposta);
+  inputResposta.addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         verificarResposta();
     }   
 });
 
-const faseAtual = 10;
-const proximaPagina = 'telaFinal.html';
+  const faseAtual = 10;
+  const proximaPagina = 'telaFinal.html';
 
-function verificarResposta() {
-    const respostaJogador = parseInt(inputResposta.value.trim());
+  function verificarResposta() {
+      const respostaJogador = parseInt(inputResposta.value.trim());
 
-    if (respostaJogador === respostaCorreta) {
-        salvarProgresso(faseAtual);
-        window.location.href = proximaPagina;
-    } else {
-        mensagemErro.style.display = 'block';
-        inputResposta.value = '';
-        inputResposta.focus();
+      if (respostaJogador === respostaCorreta) {
+          salvarProgresso(faseAtual);
+          window.location.href = proximaPagina;
+      } else {
+          mensagemErro.style.display = 'block';
+          inputResposta.value = '';
+          inputResposta.focus();
         
-        setTimeout(() => {
-            mensagemErro.style.display = 'none';
-        }, 3000);
+          setTimeout(() => {
+              mensagemErro.style.display = 'none';
+          }, 3000);
+      }
     }
-  }
-
-
